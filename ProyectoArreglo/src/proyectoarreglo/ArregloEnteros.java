4bda8914fe5485372fa0690f7d776ee416d4d4b7
mariano <mariano.a.galdeano@gmail.com>
2012-09-12 22:31:49 -0300

    mas completo

--- ProyectoArreglo/src/proyectoarreglo/ArregloEnteros.java
+++ ProyectoArreglo/src/proyectoarreglo/ArregloEnteros.java
@@ -32,13 +32,81 @@ public class ArregloEnteros {
     public int traerMenor(){
         int menor=99999;
         
-        for(int i=0; i<=arreglo.length; i++){
+        try{
+        
+        for(int i=0; i<arreglo.length; i++){
             if(arreglo[i]<menor){
                 menor=arreglo[i];
             }
         } 
+        }catch(Exception e){
+            System.out.println("Error "+e.toString());
+        }  
         return menor;
+    }
+    
+    public int traerMayor(){
+        int mayor=0;
         
+        try{
+        for(int i=0; i<arreglo.length; i++){
+            if(arreglo[i]>mayor){
+                mayor=arreglo[i];
+            }
+        }
+        }catch(Exception e){
+            System.out.println("Error "+e.toString());
+        }  
+        return mayor;
+        
+    }
+    
+    public int[] ordenarAsc(){
+        
+        int aux;
+        try{
+            for(int r=arreglo.length-1; r>=0; r--){
+                for (int i=0; i<r; i++){
+            
+                    if (arreglo[i]>arreglo[i+1]){
+                        aux=arreglo[i];
+                        arreglo[i]=arreglo[i+1];
+                        arreglo[i+1]=aux;
+                    }
+                }
+                
+            }
+          
+       }catch(Exception e){
+        System.out.println("Error "+e.toString());
+        }
+    
+       return arreglo;
+    }
+    
+     public int[] ordenarDesc(){
+       // int [] ordesc = new int [arreglo.length];
+        int aux;
+        try{
+            for(int r=0; r>arreglo.length; r++){
+                for (int i=arreglo.length-1; i>=r; i--){
+            
+                    if (arreglo[i]>arreglo[i-1]){
+                        aux=arreglo[i];
+                        arreglo[i]=arreglo[i-1];
+                        arreglo[i-1]=aux;
+                    }
+                }
+                
+            }
+         // for(int s=0; s<arreglo.length; s++){
+          //   ordesc[s]=arreglo[s];
+          //}
+       }catch(Exception e){
+        System.out.println("Error "+e.toString());
+        }
+    
+       return arreglo;
     }
     
     public int[] sumar(int[] arreglo1){
@@ -52,5 +120,108 @@ public class ArregloEnteros {
             }
         return suma;
         }
+    
+     public int[] resta(int[] arreglo1){
+        int [] resta = new int [arreglo.length];
+        try{
+            for(int i=0; i<arreglo.length; i++){
+                resta[i]=arreglo[i]-arreglo1[i];
+                }
+            }catch(Exception e){
+                System.out.println("Error "+e.toString());
+            }
+        return resta;
+        }
+     
+    public int[] dividir(int[] arreglo1){
+        int [] div = new int [arreglo.length];
+        try{
+            for(int i=0; i<arreglo.length; i++){
+                if(arreglo[i]==0){
+                    div[i]=000;
+                }else{
+                    div[i]=arreglo[i]/arreglo1[i];
+                }
+            }}catch(Exception e){
+                System.out.println("Error "+e.toString());
+            }
+        return div;
+        }
+    
+    public int calcularSuma(){
+        int suma=0;
+        try{
+            for(int i=0; i<arreglo.length; i++){
+                suma=arreglo[i]+suma;
+                }
+            }catch(Exception e){
+                System.out.println("Error "+e.toString());
+            }
+        return suma;
+        }
+    
+    public int[] invertir(){
+        int r=0;
+        int inv[] = new int [arreglo.length];
         
-    } 
+        try{    
+                for (int i=arreglo.length-1; i>=0; i--) {
+                    inv[r]=arreglo[i];
+                    r++;
+                }
+        }catch(Exception e){
+            System.out.println("Error "+e.toString());
+        }
+        return inv;
+    }
+    
+    public int[] signo(){
+        
+      try{
+        for(int i=0; i<arreglo.length; i++){
+            
+            if(arreglo[i]<0){
+                
+               arreglo[i] = -1;
+                   
+            }else
+                if(arreglo[i]==0){
+                    arreglo[i] = 0;
+                }else
+                    if(arreglo[i]>0){
+                        arreglo[i]=1;}
+        }         
+      }catch(Exception e){
+                System.out.println("Error "+e.toString());
+            }
+        return arreglo;
+    }
+    
+    public double[] calcularPromedio(){
+        
+    }
+    
+    public int[] parte(int desde, int hasta){
+        int [] part = new int [hasta-desde];
+        
+        for( int i=desde; i<=hasta; i++){
+            part[i]=arreglo[i];
+        }
+        
+        return part;
+    }
+    
+    public double[] potencia(double ex){
+        double [] pot = new double [arreglo.length];
+        
+      try{
+            for(int i=0; i<arreglo.length; i++){
+            pot[i]= Math.pow(arreglo[i], ex);
+            }
+      }catch(Exception e){
+                System.out.println("Error "+e.toString());
+            
+      }
+        return pot;
+    }
+}
--- ProyectoArreglo/src/proyectoarreglo/ProyectoArreglo.java
+++ ProyectoArreglo/src/proyectoarreglo/ProyectoArreglo.java
@@ -12,17 +12,26 @@ import java.util.Scanner;
  */
 public class ProyectoArreglo {
 
-    public static void imprimirArreglo (int [] a){
+    public static void imprimirArreglo (int a[]){
         try{
-            for(int i=0; i<a.length; i++)
-                System.out.println(a[i]+",");
-                System.out.println(a[a.length-1]);
-            
+            for(int i=0; i<a.length; i++){
+                if(a[i]==000){
+                   System.out.println ("error no se puede dividir por 0");
+                }else{
+                System.out.println(a[i]);
+                }
+            }
         }catch(Exception e){
             System.out.println("Error "+ e.toString());
         }
     }
     public static void main(String[] args) {
+        
+        int arreglo1[] = {1,2,3,4,5};
+        
+        int matriz[][] = {{3,6,1},{5,7,2}};
+        int matriz2[][] = {{5,6,2},{7,0,-2}};
+               
         Scanner in = new Scanner(System.in);
         
         System.out.println("cual es la longitud del arreglo");
@@ -30,16 +39,40 @@ public class ProyectoArreglo {
         
         int[] a = new int [n];
         
-        for(int i=0; i<=n; i++){
+        System.out.println("ingrese los " + n + " valores...");
+        
+        for(int i=0; i<n; i++){
             a[i]= in.nextInt();
         }
         
-        ArregloEnteros ae = new ArregloEnteros(a);
+        ArregloEnteros ae = new ArregloEnteros(a);      //instanciamos el objeto
         
         System.out.println("ARREGLO ENTRADA");
         
         imprimirArreglo(ae.getArreglo());
         
-        System.out.println("ARREGLO ");
+      /*  System.out.println("indique desde donde hasta donde mostrar");
+        int d= in.nextInt();
+        int h= in.nextInt();
+        System.out.println("mostrando desde "+d+" hasta "+h+"\n");
+        imprimirArreglo(ae.parte(d, h));*/
+        
+        
+        System.out.print("ordenados de forma ascendente: \n" );
+        imprimirArreglo(ae.ordenarAsc());
+        System.out.print("ordenados de forma descendiente: \n" );
+        imprimirArreglo(ae.ordenarDesc());
+        System.out.println("mayor: "+ ae.traerMayor());
+        System.out.println("menor: "+ ae.traerMenor());
+        System.out.println("signo: ");
+        imprimirArreglo(ae.signo());
+        System.out.println("suma de todos los elementos: "+ ae.calcularSuma());
+        System.out.println("suma de 2 arreglos ");
+        imprimirArreglo(ae.sumar(arreglo1));
+        System.out.println("volteado:");
+        imprimirArreglo(ae.invertir());
+        System.out.println("dividir:");
+        imprimirArreglo(ae.dividir(arreglo1));
+        
     }
 }
